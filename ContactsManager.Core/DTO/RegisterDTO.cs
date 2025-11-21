b31cac4dd8e3e96b35cb3e087cf5d98e2e3705c6
nagdeepanjan <mail@deepanjannag.com>
2025-11-21 17:20:07 -0500

    Add user role management with Admin/User support
    Introduced `UserTypeOptions` enum to define user roles (`User` and `Admin`). Updated `RegisterDTO` to include a `UserType` property for role selection during registration.
    
    Enhanced `AccountController` to handle role assignment:
    - Automatically create the `Admin` role if it doesn't exist.
    - Assign users to `Admin` or `User` roles based on the selected `UserType`.
    
    Updated `Register.cshtml` to include radio buttons for selecting the `UserType`. Adjusted `using` directives to include necessary namespaces.

--- ContactsManager.Core/DTO/RegisterDTO.cs
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactsManager.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -35,5 +36,7 @@ namespace ContactsManager.Core.DTO
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords should match")]
         public string ConfirmPassword { get; set; }
+
+        public UserTypeOptions UserType { get; set; } = UserTypeOptions.User;
     }
 }
--- ContactsManager.UI/Controllers/AccountController.cs
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using ContactsManager.Core.Domain.IdentityEntities;
 using ContactsManager.Core.DTO;
+using ContactsManager.Core.Enums;
 using CRUD_Last.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -13,11 +14,13 @@ namespace ContactsManager.UI.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         [HttpGet]
@@ -41,6 +44,25 @@ namespace ContactsManager.UI.Controllers
 
             if (result.Succeeded)
             {
+                //Check status of radiobutton (user/admin)
+                if (registerDTO.UserType == Core.Enums.UserTypeOptions.Admin)
+                {
+                    //Create 'Admin' role if it doesn't already exist
+                    if (await _roleManager.FindByNameAsync(UserTypeOptions.Admin.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole() { Name = UserTypeOptions.Admin.ToString() };
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    //Add the new user into 'Admin' role
+                    await _userManager.AddToRoleAsync(user, UserTypeOptions.Admin.ToString());
+                }
+                else
+                {
+                    //Add the new user into 'User' role
+                    //await _userManager.AddToRoleAsync(user, UserTypeOptions.User.ToString());
+                }
+
                 //Sign in
                 await _signInManager.SignInAsync(user, isPersistent: false);
 
